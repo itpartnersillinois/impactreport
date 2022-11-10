030713d200a8f854c5e29f8acf3695c5a3624f8a
Bryan Jonker <jonker@illinois.edu>
2022-11-10 16:13:36 -0600

    Fix force merge

--- gulpfile.js
+++ gulpfile.js
@@ -21,24 +21,9 @@ gulp.task("copy2020", function () {
         .pipe(gulp.dest('./_site/archive2020'));
 });
 
-<<<<<<< HEAD
-=======
-gulp.task("default", gulp.series("styles", "webfonts"));
-
->>>>>>> 6421660 (Update)
 gulp.task("copy2021", function () {
     return gulp.src('./archive2021/**')
         .pipe(gulp.dest('./_site/archive2021'));
 });
-<<<<<<< HEAD
 
 gulp.task("default", gulp.series("styles", "webfonts"));
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 86d1433 (Update)
-=======
->>>>>>> 7dcf2e4 (Update gulpfile.js)
->>>>>>> b62d195 (Update gulpfile.js)
-=======
->>>>>>> 6421660 (Update)
